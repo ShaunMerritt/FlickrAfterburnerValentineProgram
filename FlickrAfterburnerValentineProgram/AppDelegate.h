2d487d3ea07d74bb5dff56d7a8c766a6af44ef9c
Shaun Merritt <smerrittdevelopment@gmail.com>
2015-01-14 12:57:44 -0800

    Initial Commit

---
+++ FlickrAfterburnerValentineProgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrAfterburnerValentineProgram
+//
+//  Created by Shaun Merritt on 1/14/15.
+//  Copyright (c) 2015 True Merit Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrAfterburnerValentineProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrAfterburnerValentineProgram
+//
+//  Created by Shaun Merritt on 1/14/15.
+//  Copyright (c) 2015 True Merit Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
